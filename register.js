699ec984d21da3e12568c3388b4d8afc46b1f7dd
zakavo <facebook7.7.2007@gmail.com>
2026-04-19 11:59:21 +0700

    Create register.js

---
+++ register.js
@@ -0,0 +1,49 @@
+<!-- Firebase -->
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+  // DÁN config của bạn vào đây
+  const firebaseConfig = {
+  apiKey: "AIzaSyAv2N-Z-52nxkTAKsJ3iJTt_j2f3jqY4xI",
+  authDomain: "quanli-3990e.firebaseapp.com",
+  projectId: "quanli-3990e",
+  storageBucket: "quanli-3990e.firebasestorage.app",
+  messagingSenderId: "15839288744",
+  appId: "1:15839288744:web:8841eb672050790badd19d",
+  measurementId: "G-15QZWTZ36L"
+};
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  // ĐĂNG KÝ
+  window.register = function() {
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("pass").value;
+
+    createUserWithEmailAndPassword(auth, email, pass)
+      .then((userCredential) => {
+        alert("Đăng ký thành công!");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  }
+
+  // ĐĂNG NHẬP
+  window.login = function() {
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("pass").value;
+
+    signInWithEmailAndPassword(auth, email, pass)
+      .then((userCredential) => {
+        alert("Đăng nhập thành công!");
+        window.location.href = "home.html";
+      })
+      .catch((error) => {
+        alert("Sai tài khoản hoặc mật khẩu!");
+      });
+  }
+</script>
